834751b55bed5bf6849ad750c8fb8787596a11f4
Alexandr <sasha1997com@gmail.com>
2020-09-26 00:07:36 +0700

    add product

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home_url'),
     path('bouquets/', views.bouquets, name='bouquets_url'),
     path('slider/', views.slider, name='slider_url'),
+    path('bouquets_nevest/', views.bouquets_nevest, name='bouquets_nevest_url'),
+    path('compositions/', views.compositions, name='compositions_url'),
 ]
--- main/views.py
+++ main/views.py
@@ -9,5 +9,13 @@ def bouquets(request):
     return render(request, 'main/bouquets.html')
 
 
+def bouquets_nevest(request):
+    return render(request, 'main/bouquets_nevest.html')
+
+
+def compositions(request):
+    return render(request, 'main/compositions.html')
+
+
 def slider(request):
     return render(request, 'main/slider.html')
